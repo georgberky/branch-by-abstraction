ddf064daab3f3a37e8ca816a4e4f67532c4d69ca
Georg Berky <georg.berky@posteo.de>
2021-10-06 18:50:02 +0200

    add AssertJ

--- src/test/java/NothingTest.java
+++ src/test/java/NothingTest.java
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class NothingTest {
     @Test
     void nothing() {
-        
+        assertThat(true).isTrue();
     }
 }
