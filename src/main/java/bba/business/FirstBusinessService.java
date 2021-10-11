5b6c5d1cdd0ca2266a63b771ebff5ac101a59578
Georg Berky <georg.berky@posteo.de>
2021-10-11 21:17:18 +0200

    use abstraction in first service

--- src/main/java/bba/business/FirstBusinessService.java
+++ src/main/java/bba/business/FirstBusinessService.java
@@ -1,17 +1,14 @@
 package bba.business;
 
-import bba.persistence.UnreliablePersistenceStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
 
 public class FirstBusinessService {
     private static final Logger LOG = LoggerFactory.getLogger(FirstBusinessService.class);
 
-    private final UnreliablePersistenceStore store;
+    private final DataStore store;
 
-    public FirstBusinessService(UnreliablePersistenceStore store) {
+    public FirstBusinessService(DataStore store) {
         this.store = store;
 
         LOG.info("Created: {}", this.getClass().getSimpleName());
@@ -19,9 +16,9 @@ public class FirstBusinessService {
 
     public void doSomething() {
         LOG.info("Doing something first!");
-        Map<String, Object> dataFromStore = store.loadByName_legacy("name1");
+        Data dataFromStore = store.loadByName("name1");
 
-        LOG.info("Found data with name: {}", dataFromStore.get("name"));
-        LOG.info("Data has quality: {}", dataFromStore.get("quality"));
+        LOG.info("Found data with name: {}", dataFromStore.name());
+        LOG.info("Data has quality: {}", dataFromStore.quality());
     }
 }
